32d235bd89c5fc81e4d049a47f00eeca9d0f0dec
Matthew J Martinez <matthew_martinez@brown.edu>
2020-02-07 01:05:32 -0600

    Update Performance_Readability.R

--- 02_Performance_Readability/Performance_Readability.R
+++ 02_Performance_Readability/Performance_Readability.R
@@ -63,6 +63,10 @@ dat<-read.csv("https://raw.githubusercontent.com/mattdemography/STA_6233/master/
   
   #Now I want to print out a table for each of the variables I just changed. How would I do this in a loop?
   
+  #Imagine you want to print three letters (A,B,C) for every five numbers (6-10) for every name on a list (Bob, Bill, Sue, Antetokounmpo). 
+  #So the Result is Bob A6, Bob A7, Bob A8, Bob A9, Bob A10, Bob B6, Bob B7... 
+  #How would we write this?
+  
 #### While Loops ####
   #What of instead of a for loop, I want to loop while a condition is true?
   #I want to print while a calculation is below 10,000
@@ -77,19 +81,65 @@ dat<-read.csv("https://raw.githubusercontent.com/mattdemography/STA_6233/master/
   #What if I wanted to add an iteration number and the value of i to the while loop above?
   
 #### Functions ####
+  #Let's say there is a process that I want to be repeatable and callable
+  #I want to take the second power of a number then divide this by the total number or observations in the column. 
+  #Let's do this to the data we brought in earlier (dat) although it wouldn't be methodologically appropriate
   
-  testfun<-function (connection, table, joinvar, rdata)
-  {
-    p<-data.frame(rdata[,1])
-    p2<-NULL
-    j=1
-    while(j<nrow(rdata)){
-      p1<-paste0("t.", joinvar, "=", p[j,], " OR ")
-      p2 <-paste0(p2, p1)
-      j=j+1
-    }
-    rm(p1)
-    p3<-paste0("(", p2, "t.", joinvar, "=", p[nrow(rdata),], ")")
+  first_fun<-function(x,y){
+    z<-y*y
+    n<-nrow(x)
+    t<-z/n
   }
+  
+  dat$Risks_z<-dat$Risks*dat$Risks
+  head(dat[,c(2,13)])
+  dat$Risks_t<-dat$Risks_z/nrow(dat)
+  head(dat[,c(2,13:14)])
+  
+  dat$Recommend_t<-first_fun(dat, dat$Recommend)
+  head(dat[,c(3,15)])
+  
+  #How could I make the above function just one argument? Make it work
+  sec_fun<-function(x){
+    
   }
-
+  
+  dat$WorkValued_t<-sec_fun()
+  head()
+  
+  #Another Example of a function
+  #I'm Making a Fake Data Set that appoximates normality
+  ex<-rnorm(10000, mean=0, sd=1)
+  summary(ex)
+  h<-hist(ex)
+  xfit<-seq(min(ex), max(ex), length=40)
+  yfit<-dnorm(xfit, mean=mean(ex), sd=sd(ex))
+  yfit<-yfit*diff(h$mids[1:2])*length(ex)
+  lines(xfit, yfit)
+  
+  #Let's Look at our two variables. I'm creating a function for the steps above. You don't have to do this, but it can be helpful.
+  Dist_lines<-function(x){
+    h<-hist(x)
+    xfit<-seq(min(x), max(x), length=40)
+    yfit<-dnorm(xfit, mean=mean(x), sd=sd(x))
+    yfit<-yfit*diff(h$mids[1:2])*length(x)
+    lines(xfit, yfit)
+    
+  }
+  
+  #Test the Function
+  Dist_lines(ex)
+  
+  #Read the source code for each of the following three functions, puzzle out what they do, and then brainstorm better names.
+  f1 <- function(string, prefix) {
+    substr(string, 1, nchar(prefix)) == prefix
+  }
+  f2 <- function(x) {
+    if (length(x) <= 1) return(NULL)
+    x[-length(x)]
+  }
+  f3 <- function(x, y) {
+    rep(y, length.out = length(x))
+  }
+  
+#Now You will resolve a data problem using loops and functions how you see fit.
