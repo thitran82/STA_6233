9c8aa400050e433ec8994e657222e77363af4618
Matthew J Martinez <matthew_martinez@brown.edu>
2020-02-11 00:20:28 -0600

    Update Performance_Example.R

--- 02_Performance_Readability/Performance_Example.R
+++ 02_Performance_Readability/Performance_Example.R
@@ -6,7 +6,7 @@ library(dplyr)
 #Bring in the Data Dictionary from the 'Data' folder
 
 #The data dictionary file has a list of file names and years. It also contains variable names and the position of the variable in the file
-#I want to rename each variable using this information. I want to rename variables in files beginning with 'campstud' and 'campperf'
+#I want to rename each variable using this information. I want to rename variables in files beginning with 'campstud' and 'campothr'
 #for years 1995 through 2000. I also want a new variable equal to the year to be added to each file.
 
 #I want a new R object to be created for each year/file_name combination. But only the first 10 observations. 
