c202cdf4bec55d645b2e34053ff69d3247d69665
Matthew J Martinez <mjmpoetry@gmail.com>
2020-04-10 01:16:28 -0500

    adding items

--- 10_Shiny_Apps/Create_Shiny_Data.R
+++ 10_Shiny_Apps/Create_Shiny_Data.R
@@ -3,4 +3,4 @@ str(t)
 t$RottenTomatoes<-as.numeric(t$RottenTomatoes)
 
 save(t, file="./Data/nic-cage.RData")
-load("./Data/nic-cage.RData")
+load(url("https://raw.githubusercontent.com/mattdemography/STA_6233/master/Data/nic-cage.RData"))
